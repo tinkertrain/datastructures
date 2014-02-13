f733deee6e4f586bf53af82dc4c7256a15dfb56a
Juan Pablo <tinkertrain@gmail.com>
2014-02-13 17:09:28 -0600

    Removed grunt jshint task, added karma on every js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,14 +29,14 @@ module.exports = function (grunt) {
     watch: {
       js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all'],
+        tasks: ['karma'],
         options: {
           livereload: true
         }
       },
       jsTest: {
         files: ['test/spec/{,*/}*.js'],
-        tasks: ['newer:jshint:test', 'karma']
+        tasks: ['karma']
       },
       compass: {
         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
